5da9016691981c7bb1fca1892524a8a50dfe1603
Shruts0208 <79762526+Shruts0208@users.noreply.github.com>
2021-04-18 13:25:25 -0700

    Update AddData.java

--- src/edu/asu/cse360/covid360/AddData.java
+++ src/edu/asu/cse360/covid360/AddData.java
@@ -18,12 +18,6 @@ public class AddData extends VBox
 
 	public AddData()
 	{
-		Label date = new Label ("\tDate:\t\t\t\t");
-		TextField DATE = new TextField();
-		HBox hb = new HBox();
-		hb.getChildren().addAll(date,DATE);
-		hb.setSpacing(10);
-
 		Label id = new Label ("\tID:\t\t\t\t\t");
 		TextField ID = new TextField();
 		HBox hb1 = new HBox();
@@ -60,7 +54,7 @@ public class AddData extends VBox
 		hb6.getChildren().addAll(vaccineLocation,VACCINELOCATION);
 		hb5.setSpacing(10);
 		VBox vb=new VBox(8);
-		vb.getChildren().addAll(hb,hb1,hb2,hb3,hb4,hb5,hb6);
+		vb.getChildren().addAll(hb1,hb2,hb3,hb4,hb5,hb6);
 		VBox vb1 = new VBox(8);
 		Button b1=new Button("Enter");
 		vb1.getChildren().add(b1);
@@ -70,9 +64,6 @@ public class AddData extends VBox
 			public void handle(ActionEvent e)
 			{
 				String s="";
-				s=DATE.getText();
-				ob.setVaxDate(s);
-				s=id.getText();
 				int i = Integer.parseInt(s);
 				ob.setId(i);
 				s=LASTNAME.getText();
